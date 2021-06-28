6fac5275f31f193023fd99f5db5b8f1059eadb2c
gyoritamas <gyoritamasnoe@gmail.com>
2021-06-28 10:36:43 +0200

    Implement caching

--- src/main/java/com/codecool/weatherservice/WeatherServiceApplication.java
+++ src/main/java/com/codecool/weatherservice/WeatherServiceApplication.java
@@ -3,13 +3,17 @@ package com.codecool.weatherservice;
 import com.codecool.weatherservice.responsemodel.WeatherData;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.web.client.RestTemplate;
 
 /**
@@ -17,6 +21,7 @@ import org.springframework.web.client.RestTemplate;
  * http://api.openweathermap.org/data/2.5/weather?q=Budapest&appid={api_key}&units=metric&lang=hu
  */
 @SpringBootApplication
+@EnableScheduling
 @EnableCaching
 public class WeatherServiceApplication {
 
@@ -24,6 +29,15 @@ public class WeatherServiceApplication {
     private String apiKey;
 
     final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @Autowired
+    CacheManager cacheManager;
+
+    @Scheduled(fixedRate = 60000)
+    public void evictAllCacheValues(){
+        System.out.println("Clearing cache...");
+        cacheManager.getCache("weatherData").clear();
+    }
 
     @Bean
     public RestTemplate restTemplate(RestTemplateBuilder builder) {
--- src/main/java/com/codecool/weatherservice/service/WeatherService.java
+++ src/main/java/com/codecool/weatherservice/service/WeatherService.java
@@ -7,6 +7,10 @@ import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
+
 @Service
 public class WeatherService {
     private static final String urlFormat =
@@ -18,15 +22,21 @@ public class WeatherService {
     @Autowired
     private RestTemplate restTemplate;
 
-    @Cacheable("weatherdata")
+    @Cacheable(value = "weatherData")
     public String getWeatherData(String city) {
+
         printRequestsForCacheTest(city);
         final String weatherUrl = String.format(urlFormat, city, apiKey);
 
-        return restTemplate.getForObject(weatherUrl, WeatherData.class).toString();
+        return String.format("%s %s", getTime(), restTemplate.getForObject(weatherUrl, WeatherData.class).toString());
     }
 
     private void printRequestsForCacheTest(String city) {
         System.out.printf("Requesting weather data of %s...%n", city);
     }
+
+    private String getTime(){
+        return LocalDateTime.now().format(DateTimeFormatter.ofPattern("y.M.d H:m:s"));
+    }
+
 }
